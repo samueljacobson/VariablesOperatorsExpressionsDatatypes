a63cc5d0f12849d601a0781c5db841a094cdadfc
Sam Jacobson <samuel.c.jacobson@gmail.com>
2017-06-27 15:05:11 -0400

    added exercises

--- VariablesOperatorsExpressionsDatatypes/Program.cs
+++ VariablesOperatorsExpressionsDatatypes/Program.cs
@@ -10,6 +10,51 @@ namespace VariablesOperatorsExpressionsDatatypes
     {
         static void Main(string[] args)
         {
+            //initializing new firstname variable
+            string name = "Mike";
+            Console.WriteLine(name);
+
+            name = "Daniel";
+            Console.WriteLine(name);
+
+            string birthMonth = "April";
+            Console.WriteLine(birthMonth);
+
+            int age = 65;
+            string why = "because I said so";
+            char firstInitial = 'L';
+
+            //floating point types
+            float floatNumber = 2.164403304897294849383961032f;
+            Console.WriteLine(floatNumber);
+
+            double doubleNumber = 2.164403304897294849383961032d;
+            Console.WriteLine(doubleNumber);
+
+            decimal decimalNumber = 2.164403304897294849383961032m;
+            Console.WriteLine(decimalNumber);
+
+            int jessicaAge = 23;
+            int samAge = 47;
+            Console.WriteLine(jessicaAge < samAge);
+
+            Console.WriteLine("What is the name on the reservation?");
+            string reserveName = Console.ReadLine();
+
+            Console.WriteLine("How many people are in your party?");
+            int numberOfPeople = int.Parse(Console.ReadLine());
+
+            double amountEach = 13.53d;
+            double totalBill = numberOfPeople * amountEach;
+            Console.WriteLine(totalBill);
+
+            Console.WriteLine("Reservation: " + reserveName);
+            Console.WriteLine("Total Due: $" + totalBill);
+
+
+
+
+
         }
     }
 }
